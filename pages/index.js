a23eb57ae8a24ea710f656bd0dc2e0fc1d8c0f13
sven-so <svenso@web.de>
2022-11-30 23:22:41 +0100

    first exercise completed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -11,11 +16,25 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>quick-witted answer trainer 3001</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>get automatic good answers, so you don't need to overthink yourself</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+        <textarea className="prompt-box" 
+                  placeholder="start typing here"  
+                  value={userInput}
+                  onChange={onUserChangedText}
+        />;
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
+        </div>
         </div>
       </div>
       <div className="badge-container grow">
